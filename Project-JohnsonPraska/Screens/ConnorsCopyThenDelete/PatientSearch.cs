c54247058d1386a2513821a8dae4c8b63327d8c1
Matthew Johnson <mhuajij@gmail.com>
2025-04-21 18:36:33 -0500

    Connors stuff

---
+++ Project-JohnsonPraska/Screens/ConnorsCopyThenDelete/PatientSearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_JohnsonPraska.Screens.Patient
+{
+    public partial class PatientSearch : Form
+    {
+        public PatientSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFilterName_Click(object sender, EventArgs e)
+        {
+            //Code to filter search results by Name
+        }
+
+        private void btnFilterAge_Click(object sender, EventArgs e)
+        {
+            //Code to filter search results by Age
+        }
+
+        private void btnFilterRoom_Click(object sender, EventArgs e)
+        {
+            //Code to filter search results by Room
+        }
+
+        private void btnFilterStatus_Click(object sender, EventArgs e)
+        {
+            //Code to filter search results by Room
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //Code to add a new patient
+        }
+
+        private void btnAddPatient_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
