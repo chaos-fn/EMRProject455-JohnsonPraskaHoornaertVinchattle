afffb37d816c1f9c1f81c505e1de16a1b1f65035
connor-praska <connor.praska@hotmail.com>
2025-05-02 18:36:34 -0500

    Finished Patient Search screen

--- Project-JohnsonPraska/Screens/Search/Search.cs
+++ Project-JohnsonPraska/Screens/Search/Search.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Project_JohnsonPraska.Global;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Project_JohnsonPraska
 {
@@ -72,33 +73,84 @@ namespace Project_JohnsonPraska
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            //Code here to search for patient's by name
+            DataTable dt = new DataTable();
+            dt = SearchService.SearchByName(txtSearch.Text.ToString());
+
+            FillGrid(dataPatients, dt);
         }
 
         private void btnFilterName_Click(object sender, EventArgs e)
         {
-            //Code here to filter by Name
+            DataTable dt = new DataTable();
+            dt = SearchService.GetPatients("Fname");
+
+            FillGrid(dataPatients, dt);
         }
 
         private void btnFilterAge_Click(object sender, EventArgs e)
         {
-            //Code here to filter by Name
+            DataTable dt = new DataTable();
+            dt = SearchService.GetPatients("Date_Of_Birth");
+
+            FillGrid(dataPatients, dt);
         }
 
         private void btnFilterRoom_Click(object sender, EventArgs e)
         {
-            //Code here to filter by Name
-        }
+            DataTable dt = new DataTable();
+            dt = SearchService.GetPatients("Room");
 
-        private void btnFilterStatus_Click(object sender, EventArgs e)
-        {
-            //Code here to filter by Name
+            FillGrid(dataPatients, dt);
         }
 
         private void btnAddPatient_Click(object sender, EventArgs e)
         {
             Patients reg = new Patients();
             reg.Show();
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = SearchService.GetPatients("Patient_ID");
+
+            FillGrid(dataPatients, dt);
+        }
+
+        private void dataPatients_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Code here to view selected patient profile
+        }
+
+        private void FillGrid(DataGridView grid, DataTable data)
+        {
+            grid.Rows.Clear();
+
+            foreach (DataRow row in data.Rows)
+            {
+                int index = grid.Rows.Add(
+                    row["Patient_ID"].ToString(),
+                    row["Fname"].ToString() + " " + row["Lname"].ToString(),
+                    GetAge(row),
+                    row["Room"].ToString()
+                );
+            }
+
+        }
+
+        private static Int32 GetAge(DataRow row)
+        {
+            DateTime today = DateTime.Today;
+            DateTime birthdate = Convert.ToDateTime(row["Date_Of_Birth"]);
+            Int32 age = today.Year - birthdate.Year;
+
+            if (birthdate.Date > today.AddYears(-age))
+            {
+                return (age--);
+            } else
+            {
+                return age;
+            }
         }
     }
 }
---
+++ Project-JohnsonPraska/Screens/Search/SearchService.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using Project_JohnsonPraska.Screens.Prescriptions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_JohnsonPraska
+{
+    public static class SearchService
+    {
+        private static readonly string connectionString = "server=localhost;port=3306;user id=appuser;password=password;database=emr;";
+
+        public static DataTable GetPatients(string filter)
+        {
+            DataTable dt = new DataTable();
+            string order = "ASC";
+            if (filter == "Date_Of_Birth") order = "DESC";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @$"SELECT Patient.Patient_ID,
+                                        Patient.Fname,
+                                        Patient.Lname,
+                                        Patient.Date_Of_Birth,
+                                        Patient.Room
+                                 FROM Patient
+                                 ORDER BY {filter} {order}";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
+        public static DataTable SearchByName(string name)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @$"SELECT Patient_ID,
+                                         Fname,
+                                         Lname,
+                                         Date_Of_Birth,
+                                         Room
+                                 FROM Patient
+                                 WHERE Fname LIKE ""{name}%""";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+    }
+}
+
